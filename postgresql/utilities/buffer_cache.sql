90595c1c65e3b36355b0ddb17c09ffa9b0d10514
stephanesql <stephane.lorek@gmail.com>
2020-03-27 11:39:51 +0100

    Add list schemas

--- postgresql/utilities/buffer_cache.sql
+++ postgresql/utilities/buffer_cache.sql
@@ -344,6 +344,15 @@ SELECT
       WHERE sml.relpages - otta > 0 OR ipages - iotta > 10
       ORDER BY wastedbytes DESC, wastedibytes DESC;
 
-
+/**********************************
+List schemas
+***********************************/
+ SELECT n.nspname AS "Name",                                          
+   pg_catalog.pg_get_userbyid(n.nspowner) AS "Owner",                 
+   pg_catalog.array_to_string(n.nspacl, E'\n') AS "Access privileges",
+   pg_catalog.obj_description(n.oid, 'pg_namespace') AS "Description" 
+ FROM pg_catalog.pg_namespace n                                       
+ WHERE n.nspname !~ '^pg_' AND n.nspname <> 'information_schema'      
+ ORDER BY 1;
 
   
